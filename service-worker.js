b906ba5b1b5174c8ca771fc829b615a63e333824
Agent Theo <july01keartonhill@gmail.com>
2025-06-13 22:32:32 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,37 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('game-cache').then((cache) => {
-      return cache.addAll(['index.html']);
+const CACHE_NAME = 'site-cache-v1';
+const urlsToCache = [
+  '/index.html',
+  '/icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
